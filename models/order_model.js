c183302da0a309151ae229b4ae036cecd0d36e3c
Jordan-Burgess <alfredjordan93@gmail.com>
2023-12-21 09:24:57 -0500

    Update price_model and added order model

---
+++ models/order_model.js
@@ -0,0 +1,18 @@
+orders = [
+    {
+        productName: 'iPhone 14',
+        toAddress: '123 Home Street',
+        toCountry: 'Congo',
+        toCity: 'Kinshasa',
+        fromAddress: '567 Rose Drive',
+        fromCountry: 'USA',
+        fromCity: 'New York',
+        weight: 0.45,
+        productValue: 729.00,
+        shippingPrice: 120,
+        recieverName: 'Delmar Smith',
+        recieverPhoneNumber: '123-456-1243',
+        senderName: 'Jordan Smart',
+        senderPhoneNumber: '572-120-1043',
+    }
+]
